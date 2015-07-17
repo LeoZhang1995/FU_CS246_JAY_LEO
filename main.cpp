0d4425ccaa06169b22006ff16fb3842fa7e2ed0b
JiaJun Zhou <jiajunzhou@JiaJundeMacBook-Pro.local>
2015-07-16 20:44:50 -0400

    fuck u

--- main.cpp
+++ main.cpp
@@ -7,7 +7,29 @@
 //
 
 #include <iostream>
-#include 
+#include "Game_Controller.h"
+#include "AI_Four.h"
+#include "Game_Controller.h"
+#include "Pawn.h"
+#include "AI_One.h"
+#include "Game_Setup.h"
+#include "Player.h"
+#include "AI_Three.h"
+#include "Game_View.h"
+#include "Queen.h"
+#include "AI_Two.h"
+#include "Graphical_Display.h"
+#include "Rook.h"
+#include "Bishop.h"
+#include "Human.h"
+#include "Text_Display.h"
+#include "Black_Block.h"
+#include "King.h"
+#include "White_Block.h"
+#include "Board.h"
+#include "Knight.h"
+#include "Chess.h"
+#include "Move.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
